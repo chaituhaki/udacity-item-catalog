01b10289a223d279b46015697197710e0e2be7f4
chaitanya LN <chaituhaki@users.noreply.github.com>
2017-11-01 18:23:26 -0400

    Created Genre, Item, User models

--- database_setup.py
+++ database_setup.py
@@ -8,6 +8,9 @@ from passlib.apps import custom_app_context
 Base = declarative_base()
 
 class User(Base):
+    """
+    Stores user details
+    """
     __tablename__ = "user"
 
     id = Column(Integer, primary_key = True)
@@ -20,24 +23,34 @@ class User(Base):
     def verify_password(self, password):
         return custom_app_context.verify(password, self.password_hash)
 
-class Services(Base):
-    __tablename__ = "services"
+class Genre(Base):
+    """
+    List of Anime Genres
+    """
+    __tablename__ = "genre"
     
     id = Column(Integer, primary_key = True)
-    name = Column(String(30), nullable = False)
-    description = Column(String(250))
+    name = Column(String(20), nullable = False)
+
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
 
-class ServiceItem(Base):
-    __tablename__ = "service_item"
+class Item(Base):
+    """
+    Stores properties of each Anime
+    """
+    __tablename__ = "item"
 
     id = Column(Integer, primary_key = True)
     name = Column(String(30), nullable = False)
     description = Column(String(250))
-    service_id = Column(Integer, ForeignKey('services.id'))
-    services = relationship(Services)
+
+    genre_id = Column(Integer, ForeignKey('genre.id'))
+    genre = relationship(Genre)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
 
-engine = create_engine("sqlite:///ItemCatalog.db")
+
+engine = create_engine("sqlite:///AnimeCatalog.db")
 Base.metadata.create_all(engine)
