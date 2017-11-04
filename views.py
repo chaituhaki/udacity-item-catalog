c9d4a322e66a592d201953cab01c12d15bc019df
chaitanya LN <chaituhaki@users.noreply.github.com>
2017-11-04 19:56:26 -0400

    added user validation for every CRUD opeartion

--- views.py
+++ views.py
@@ -14,7 +14,9 @@ import httplib2
 import json
 from flask import make_response
 import requests
-import random, string
+import random
+import string
+
 
 engine = create_engine("sqlite:///AnimeCatalog.db")
 
@@ -24,7 +26,8 @@ session = DBSession()
 app = Flask(__name__)
 
 
-CLIENT_ID = json.loads(open('client_secrets.json', 'r').read())['web']['client_id']
+CLIENT_ID = json.loads(open('client_secrets.json', 'r')
+                       .read())['web']['client_id']
 
 
 @app.route('/gconnect', methods=['POST'])
@@ -46,7 +49,7 @@ def gconnect():
             json.dumps('Failed to upgrade the authorization code.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
-    
+
     # Check that the access token is valid.
     access_token = credentials.access_token
     url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
@@ -59,7 +62,7 @@ def gconnect():
         response = make_response(json.dumps(result.get('error')), 500)
         response.headers['Content-Type'] = 'application/json'
         return response
-    
+
     # Verify that the access token is used for the intended user.
     gplus_id = credentials.id_token['sub']
     if result['user_id'] != gplus_id:
@@ -75,15 +78,15 @@ def gconnect():
         print ("Token's client ID does not match app's.")
         response.headers['Content-Type'] = 'application/json'
         return response
-    
+
     stored_access_token = login_session.get('access_token')
     stored_gplus_id = login_session.get('gplus_id')
     if stored_access_token is not None and gplus_id == stored_gplus_id:
-        response = make_response(json.dumps('Hi, already logged in'),
-                                 200)
+        response = make_response(json.dumps('Hi ' + login_session[
+                                 'username'] + ', already logged in'), 200)
         response.headers['Content-Type'] = 'application/json'
         return response
-    
+
     # Store the access token in the session for later use.
     login_session['access_token'] = credentials.access_token
     login_session['gplus_id'] = gplus_id
@@ -94,7 +97,7 @@ def gconnect():
     answer = requests.get(userinfo_url, params=params)
 
     data = answer.json()
-    
+
     login_session['username'] = data['name']
     login_session['picture'] = data['picture']
     login_session['email'] = data['email']
@@ -106,10 +109,8 @@ def gconnect():
     if not user_id:
         user_id = createUser(login_session)
     login_session['user_id'] = user_id
-
-    
-    return jsonify(name=login_session['username'], email=login_session['email'],
-                   img=login_session['picture'])
+    return jsonify(name=login_session['username'], email=login_session[
+                   'email'], picture=login_session['picture'])
 
 # User Helper Functions
 
@@ -153,16 +154,17 @@ def gdisconnect():
     print (login_session['username'])
     if access_token is None:
         print ('Access Token is None')
-        response = make_response(json.dumps('Current user not connected.'), 401)
+        response = make_response(json.dumps('Current user not connected.'),
+                                 401)
         response.headers['Content-Type'] = 'application/json'
         return response
-    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % login_session['access_token']
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s'\
+        % login_session['access_token']
     h = httplib2.Http()
     result = h.request(url, 'GET')[0]
-    print ('result is ')
-    print (result)
     if result['status'] == '200':
-        del login_session['access_token'] 
+        print("deleting user session")
+        del login_session['access_token']
         del login_session['gplus_id']
         del login_session['username']
         del login_session['email']
@@ -171,20 +173,10 @@ def gdisconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
     else:
-	
-    	response = make_response(json.dumps('Failed to revoke token for given user.', 400))
-    	response.headers['Content-Type'] = 'application/json'
-    	return response
-
-
-@app.route('/')
-@app.route('/genre/')
-def showGenre():
-    state = create_state()
-    allGenre = session.query(Genre).all()
-    item = session.query(Item).filter_by(id=1).all()
-    state = create_state()
-    return render_template('home.html', allGenre=allGenre, item=item, STATE=state)
+        response = make_response(json.dumps(
+                    'Failed to revoke token for given user.', 400))
+        response.headers['Content-Type'] = 'application/json'
+        return response
 
 
 @app.errorhandler(404)
@@ -197,10 +189,11 @@ def get_genre():
     """
     returns json that contains all genre categories
     """
-    if 'username' not in login_session['username']:
-        return redirect('/login')
-    genre = session.query(Genre).all()
-    return jsonify(genre=[g.serialize for g in genre])
+    if 'username' in login_session:
+        genre = session.query(Genre).all()
+        return jsonify(genre=[g.serialize for g in genre])
+    else:
+        return redirect(url_for('showGenre'))
 
 
 @app.route('/items.json/')
@@ -208,69 +201,122 @@ def get_items():
     """
     returns json that contains all anime items
     """
-    items = session.query(Item).all()
-    return jsonify(items=[i.serialize for i in items])
+    if 'username' in login_session:
+        items = session.query(Item).all()
+        return jsonify(items=[i.serialize for i in items])
+    else:
+        redirect(url_for('showGenre'))
+
+
+@app.route('/')
+@app.route('/genre/')
+def showGenre():
+    """
+    shows all anime genre with newly added anime
+    """
+    allGenre = session.query(Genre).all()
+    item = session.query(Item).order_by("id desc").limit(6)
+    if 'username' in login_session:
+        return render_template('LatestItems.html', allGenre=allGenre,
+                               item=item, username=login_session['username'],
+                               STATE=login_session['state'],
+                               CLIENT_ID=CLIENT_ID)
+    else:
+        state = create_state()
+        return render_template('LatestItems.html', allGenre=allGenre,
+                               item=item, STATE=login_session['state'],
+                               CLIENT_ID=CLIENT_ID)
 
 
 @app.route('/genre/<int:genre_id>/items/')
 def showItems(genre_id):
+    """
+    shows all anime genre with specific anime list
+    """
     allGenre = session.query(Genre).all()
-    genre = session.query(Genre).filter_by(id=genre_id).one()
-    item = session.query(Item).filter_by(genre_id=genre_id).all()
-    return render_template('items.html', genre=genre,
-                           item=item, allGenre=allGenre)
+    if 'username' in login_session:
+        genre = session.query(Genre).filter_by(id=genre_id).one()
+        item = session.query(Item).filter_by(genre_id=genre_id).all()
+        return render_template('items.html', genre=genre, allGenre=allGenre,
+                               item=item, username=login_session['username'],
+                               CLIENT_ID=CLIENT_ID)
+    else:
+        state = create_state()
+        item = session.query(Item).order_by("id desc").limit(6)
+        return render_template('LatestItems.html', allGenre=allGenre,
+                               STATE=state, CLIENT_ID=CLIENT_ID, item=item,
+                               error="Please Login")
 
 
 # Add new Item
 @app.route('/genre/<int:genre_id>/items/new/', methods=['GET', 'POST'])
 def newItem(genre_id):
-    genre = session.query(Genre).filter_by(id=genre_id).one()
-    # POST request
-    if request.method == 'POST':
-        if request.form['name']:
-            item = Item(name=request.form['name'], genre_id=genre_id)
-            session.add(item)
-            session.commit()
-            return redirect(url_for('showItems', genre_id=genre_id))
+    """
+    Add new anime item in list 
+    """
+    if 'username' in login_session:
+        genre = session.query(Genre).filter_by(id=genre_id).one()
+        # POST request
+        if request.method == 'POST':
+            if request.form['name']:
+                item = Item(name=request.form['name'], genre_id=genre_id)
+                session.add(item)
+                session.commit()
+                return redirect(url_for('showItems', genre_id=genre_id))
+            else:
+                alert = 'alert("Fill all required field")'
+                return render_template('newItem.html',
+                                       alert=alert, genre=genre)
+        # GET request
         else:
-            alert = 'alert("Fill all required field")'
-            return render_template('newItem.html', alert=alert, genre=genre)
-    # GET request
+            return render_template('newItem.html', genre=genre)
     else:
-        return render_template('newItem.html', genre=genre)
+        return redirect(url_for('showItems', genre_id=genre_id))
 
 
 # EDIT a Item
 @app.route('/genre/<int:genre_id>/items/<int:item_id>/edit/',
            methods=['GET', 'POST'])
 def editItem(genre_id, item_id):
-    genre = session.query(Genre).filter_by(id=genre_id).one()
-    item = session.query(Item).filter_by(id=item_id).one()
-    # POST request
-    if request.method == 'POST':
-        if request.form['name']:
-            item.name = request.form['name']
-            session.add(item)
-            session.commit()
-        return redirect(url_for('showItems', genre_id=genre_id))
-    # GET request
+    """
+    edit existing anime name
+    """
+    if 'username' in login_session:
+        genre = session.query(Genre).filter_by(id=genre_id).one()
+        item = session.query(Item).filter_by(id=item_id).one()
+        # POST request
+        if request.method == 'POST':
+            if request.form['name']:
+                item.name = request.form['name']
+                session.add(item)
+                session.commit()
+            return redirect(url_for('showItems', genre_id=genre_id))
+        # GET request
+        elif request.method == 'GET':
+            return render_template('editItem.html', item=item)
     else:
-        return render_template('editItem.html', item=item)
+        return redirect(url_for('showItems', genre_id=genre_id))
 
 
 @app.route('/genre/<int:genre_id>/items/<int:item_id>/delete/',
            methods=['GET', 'POST'])
 def deleteItem(genre_id, item_id):
-    genre = session.query(Genre).filter_by(id=genre_id).one()
-    item = session.query(Item).filter_by(id=item_id).one()
-    # POST request
-    if request.method == 'POST':
-        session.delete(item)
-        session.commit()
-        return redirect(url_for('showItems', genre_id=genre_id))
-    # GET request
+    """
+    delete anime from the list
+    """
+    if 'username' in login_session:
+        genre = session.query(Genre).filter_by(id=genre_id).one()
+        item = session.query(Item).filter_by(id=item_id).one()
+        # POST request
+        if request.method == 'POST':
+            session.delete(item)
+            session.commit()
+            return redirect(url_for('showItems', genre_id=genre_id))
+        # GET request
+        else:
+            return render_template('deleteItem.html', item=item, genre=genre)
     else:
-        return render_template('deleteItem.html', item=item, genre=genre)
+        return redirect(url_for('showItems', genre_id=genre_id))
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
